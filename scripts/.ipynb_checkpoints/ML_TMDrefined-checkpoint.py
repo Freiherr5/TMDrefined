553f1a84aa415eb9ebbdd26fd55449bd768a8f4c
Freiherr5 <wernerfeiler@gmx.de>
2024-03-24 06:30:45 +0100

    changes

--- scripts/.ipynb_checkpoints/ML_TMDrefined-checkpoint.py
+++ scripts/.ipynb_checkpoints/ML_TMDrefined-checkpoint.py
@@ -64,13 +64,8 @@ class ForestTMDrefind:
 
         # output of search:
         best_params = search_cv.best_params_
-
-        best_params_txt = best_params.copy()
-        for key, value in best_params_txt.items():
-            best_params_txt[key] = [value]
-
         with open(f"{path_forest}{sep}{job_name}_best_params.txt", "w") as file:
-            file.write(f"{best_params_txt}")
+            file.write(f"{best_params}")
 
         # new model with best_params
         clf_best = RandomForestClassifier(**best_params)
@@ -128,8 +123,8 @@ class ForestTMDrefind:
                                  'Conformation': "#3d9f47",
                                  'Energy': "#d54141"
                                  }
-        path, sep = find_folderpath()
-        path_to_scales_df = f"{path.split("scripts")[0]}{sep}_scales{sep}scales_cat.xlsx"
+
+        path_to_scales_df = f"{path.split("scripts")[0]}_scales{sep}scales_cat.xlsx"
         # scale_cat allows for mapping of categories
         scale_cat_df = pd.read_excel(path_to_scales_df).set_index("scale_id")
 
@@ -179,11 +174,7 @@ class ForestTMDrefind:
         # Create the confusion matrix
         cm = confusion_matrix(label_test, label_pred)
         if cm_save:
-            path, sep = find_folderpath()
             date_today = date.today()
-            layout = type('IdentityClassifier', (), {"predict": lambda i: i, "_estimator_type": "classifier"})
-            cm = ConfusionMatrixDisplay.from_estimator(layout, label_pred, label_test, normalize='true',
-                                                       values_format='.2%')
             cm.figure_.savefig(f"{self.path_forest}{sep}{self.job_name}_confusion_matrix_{date_today}.png")
         else:
             ConfusionMatrixDisplay(confusion_matrix=cm).plot()
@@ -204,40 +195,8 @@ class ForestTMDrefind:
                                                  label_df=df_sequence_windows_filter["label"],
                                                  scale_df_filter=scale_labels)
         pred_proba_window = self.model.predict_proba(scale_df.to_numpy().tolist()).tolist()
-        pos_proba_pre = [pred[1] for pred in pred_proba_window]
-        # shuffler method to align pos_proba-pre with sequence: 0, -1, 1, -2, 2....
-        pos_proba = []
-        flag_set = True  # the switch
-        for proba in pos_proba_pre:
-            if flag_set:
-                pos_proba.append(proba)
-                flag_set = False
-            else:
-                pos_proba.insert(0, proba)
-                flag_set = True
-
-        max_index = pos_proba.index(max(pos_proba))
+        max_index = pred_proba_window.index(max(pred_proba_window))
         best_pos = pos_seq_list[max_index]
-
-        # visualize
-        start = int(pos_intersect)
-        if self.start_tmd:
-            start = int(pos_intersect-1)
-        seq_slice_list = list(sequence[start-11: start+12])
-        fig2, ax2 = plt.subplots()
-        array24 = np.linspace(1,23,23, dtype=int)
-        ax2.bar(array24, pos_proba, width=0.5)
-        ax2.set_xticks(array24)
-        ax2.set_xticklabels(seq_slice_list, rotation=0, ha="center", fontweight="bold")
-        ax2.set_title(f"{entry_tag} TMD|JMD intersection probability", fontsize=20, fontweight="bold")
-        ax2.spines[['right', 'top']].set_visible(False)
-        ax2.set_xlabel('sequence', fontweight="bold")
-        ax2.set_ylabel('probability', fontweight="bold")
-        # saving
-        path, sep = find_folderpath()
-        date_today = date.today()
-        plt.savefig(f"{self.path_forest}{sep}{self.job_name}_{entry_tag}_proba_intersect_{date_today}.png", dpi=400,
-                    bbox_inches="tight")
         return [entry_tag, best_pos]
 
 
